d8500e0e61f0cf8d5f881da0faa4b41d01586a56
Nicholi Caron <nmcaron@protonmail.ch>
2023-02-26 18:10:48 -0500

    Lab 7B

--- labs/LAB7/Lab7B.cpp
+++ labs/LAB7/Lab7B.cpp
@@ -4,10 +4,24 @@ Section: WE1
 Term: Spring '23
 Instructor: Nick Murphy
 Name: Nicholi Moore Caron
-Lab#: 7B
+Lab#: 7B: Right-Triangle
 */
 
+#include <iostream>
+using namespace std;
+
 int main(int argc, char ** argv) {
+	int dim;
+	cout << "Please enter a value for the size: ";
+	cin >> dim;
+
+	cout << "This is the requested " << dim << 'x' << dim << " right-triangle:\n";
+	for (int outer = 1; outer <= dim; outer++) {
+		for (int inner = 0; inner < outer; inner++) {
+			cout << '*';
+		}
+		cout << '\n';
+	}
 
 	return 0;
 }
